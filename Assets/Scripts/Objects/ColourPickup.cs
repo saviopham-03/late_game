42c2020cd8517c95f60b638109c76ca15cc5b023
i eat glue <bemyfwendpweaz@gmail.com>
2026-08-15 22:09:04 +1000

    added orb take animation to colour orb

--- Assets/Scripts/Objects/ColourPickup.cs
+++ Assets/Scripts/Objects/ColourPickup.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class ColourPickup: MonoBehaviour
 {
-    [SerializeField] 
-    private PlayerColour pickupColour;
+    [SerializeField] private PlayerColour pickupColour;
+    [SerializeField] private Animator _animator;
+    private bool active = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!active) return;
+        active = false;
         PlayerColourController playerColourController = other.GetComponent<PlayerColourController>();
         if(playerColourController == null)
         {
             return;
         }
         playerColourController.SetColour(pickupColour);
-        Destroy(gameObject);
+        _animator.SetTrigger("pickup");
     }
 
 }
