a721ed028b8098c4521a381fe6f68c70a8fd0f0e
i eat glue <bemyfwendpweaz@gmail.com>
2026-09-19 17:33:12 +1000

    Added slight movement to camera change so its less jarring

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using static System.Math;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
     [SerializeField] private BoxCollider2D startingCameraPoint;
+    [SerializeField] private float cameraSwapSpeed;
+    private Bounds bounds;
+    private bool swap = false;
+    private float sizeFromHeight;
+    private float sizeFromWidth;
 
     private void Start()
     {
@@ -12,31 +18,45 @@ public class CameraManager : MonoBehaviour
             SwitchCamera(startingCameraPoint);
         }
     }
+    
+    private void Update(){
+        if (swap)
+        {
+            float new_x = Mathf.Lerp(mainCamera.transform.position.x, bounds.center.x, cameraSwapSpeed);
+            float new_y = Mathf.Lerp(mainCamera.transform.position.y, bounds.center.y, cameraSwapSpeed);
+
+            float zoom = Mathf.Lerp(mainCamera.orthographicSize, sizeFromHeight, cameraSwapSpeed);
+
+            mainCamera.transform.position = new Vector3(
+            new_x,
+            new_y,
+            mainCamera.transform.position.z
+            );
+
+            mainCamera.orthographicSize = zoom;
+        
+            if (Abs(new_x-bounds.center.x) <= 0.1 && Abs(new_x-bounds.center.x) <= 0.1 && Abs(mainCamera.orthographicSize-sizeFromHeight) <= 0.1)
+            {
+                mainCamera.transform.position = new Vector3(
+                bounds.center.x,
+                bounds.center.y,
+                mainCamera.transform.position.z
+                );
+                mainCamera.orthographicSize = sizeFromHeight;
+                swap = false;
+            }
+        }
+    }
 
     public void SwitchCamera(BoxCollider2D targetPoint)
     {
         CloneManager.Instance.switchCloneSet(targetPoint);
-        Bounds bounds = targetPoint.bounds;
+        bounds = targetPoint.bounds;
 
-        // Center camera on rectangle
-        mainCamera.transform.position = new Vector3(
-            bounds.center.x,
-            bounds.center.y,
-            mainCamera.transform.position.z
-        );
+        sizeFromHeight = bounds.size.y / 2f;
+        sizeFromWidth = bounds.size.x / (2f * mainCamera.aspect);
 
-        // Calculate orthographic size
-        float height = bounds.size.y;
-        float width = bounds.size.x;
+        swap = true;
 
-        float aspect = mainCamera.aspect;
-
-        float sizeFromHeight = height / 2f;
-        float sizeFromWidth = width / (2f * aspect);
-
-        mainCamera.orthographicSize = Mathf.Max(
-            sizeFromHeight,
-            sizeFromWidth
-        );
     }
 }
